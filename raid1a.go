142735642a023a138709c48928d55a3ee90d444e
Shynghys <shynghys@mail.ru>
2019-10-19 17:19:52 +0600

    raid1a

--- raid1a.go
+++ raid1a.go
@@ -1,19 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/01-edu/z01"
+)
 
 func Raid1a(rows int, columns int) {
 	for i := 1; i <= columns; i++ {
 		for j := 1; j <= rows; j++ { //
 			if (i == 1 && j == rows) || (i == 1 && j == 1) ||
 				(i == columns && j == 1) || (i == columns && j == rows) {
-				fmt.Print("o")
+				z01.PrintRune(111)
 			} else if i != 1 && j == 1 || i != columns && j == rows {
-				fmt.Print("|")
+				z01.PrintRune(124)
 			} else if i == 1 || j == 1 || i == columns || j == rows {
-				fmt.Print("-")
+				z01.PrintRune(45)
 			} else {
-				fmt.Print(" ")
+				z01.PrintRune(32)
 			}
 
 		}
