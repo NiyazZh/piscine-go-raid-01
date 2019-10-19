19e5384281c59a05a48f420b61ddb4d65c59b57d
NiyazZh <niyazzharassov@gmail.com>
2019-10-19 16:54:46 +0600

    test

--- raid1a.go
+++ raid1a.go
@@ -1,36 +1,32 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-	
-)
+import 	"github.com/01-edu/z01"
 
 func Raid1a(x, y int) {
 	a := "o"
 	b := "-"
-if y>1 {
+	if y > 1 {
 
+		parteCimaBaixo(a, b, x)
 
-parteCimaBaixo(a,b, x)
+		//-------------------------------------------------
 
-	//-------------------------------------------------
+		c := "|"
+		d := " "
+		lados(x, y, c, d)
+		parteCimaBaixo(a, b, x)
+	} else {
+		parteCimaBaixo(a, b, x)
+	}
 
-	c := "|"
-	d := " "
-lados(x,y,c,d)
-	parteCimaBaixo(a,b, x)
-}else {
-	parteCimaBaixo(a,b,x)
 }
-
-}
-func parteCimaBaixo(a,b string, x int ){
-	if x>1{
+func parteCimaBaixo(a, b string, x int) {
+	if x > 1 {
 		for i := x; i <= x; i++ {
 			Printrunes2(a, 0)
 			for f := 1; f <= x-2; f++ {
 				Printrunes2(b, 0)
-	
+
 			}
 		}
 		Printrunes2(a, 0)
@@ -39,37 +35,37 @@ func parteCimaBaixo(a,b string, x int ){
 	}
 	z01.PrintRune('\n')
 }
-func lados(x, y int, c,d string ){
-	if x>1{
+func lados(x, y int, c, d string) {
+	if x > 1 {
 		for l := 0; l < y-2; l++ {
-	
+
 			Printrunes2(c, 0)
-				for h := 0; h < x-2; h++ {
-					Printrunes2(d, 0)
-	
+			for h := 0; h < x-2; h++ {
+				Printrunes2(d, 0)
+
 			}
 			Printrunes2(c, 0)
 			z01.PrintRune('\n')
 		}
-	}else {
+	} else {
 		for l := 0; l < y-2; l++ {
-	
+
 			Printrunes2(c, 0)
 			z01.PrintRune('\n')
 		}
-	
+
 	}
 }
-func Printrunes2(a string, s int){
+func Printrunes2(a string, s int) {
 
-	if a!= "" && s==0{
-		p:=[]rune(a)
-		for _,l:= range p{
+	if a != "" && s == 0 {
+		p := []rune(a)
+		for _, l := range p {
 			z01.PrintRune(l)
 
 		}
 	} else {
 		PrintNbr(s)
 	}
-	
-}  
+
+}
